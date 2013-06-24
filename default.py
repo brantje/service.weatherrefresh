6c228c67e103d3a2653d07a3b7b612e81ec61734
brantje <brantje@gmail.com>
2013-06-24 08:45:07 +0000

    Initial upload

---
+++ default.py
@@ -0,0 +1,14 @@
+import xbmc
+import xbmcaddon
+import time
+__addon__      = xbmcaddon.Addon()
+__addonname__   = __addon__.getAddonInfo('name')
+__icon__        = __addon__.getAddonInfo('icon')
+
+
+while True: 
+	line1 = "Weather updated"
+	thetime = 5000  #in miliseconds
+	xbmc.executebuiltin('Notification(%s, %s, %d, %s)'%(__addonname__,line1, thetime, __icon__))
+	xbmc.executebuiltin('Weather.Refresh')
+	time.sleep( 900 )
