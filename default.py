6e237f6500a7f8cb1228f6f7caca367b0725051d
brantje <brantje@gmail.com>
2013-06-24 08:56:54 +0000

    Remove notification

--- default.py
+++ default.py
@@ -9,6 +9,6 @@ __icon__        = __addon__.getAddonInfo('icon')
 while True: 
 	line1 = "Weather updated"
 	thetime = 5000  #in miliseconds
-	xbmc.executebuiltin('Notification(%s, %s, %d, %s)'%(__addonname__,line1, thetime, __icon__))
+	#xbmc.executebuiltin('Notification(%s, %s, %d, %s)'%(__addonname__,line1, thetime, __icon__))
 	xbmc.executebuiltin('Weather.Refresh')
 	time.sleep( 900 )
